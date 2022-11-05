dc2e18fa55f13a2c34d79bcfa4a7bf463acff327
Uggla <uggla@free.fr>
2022-11-05 18:56:10 +0100

    feat: Update aerial config

--- init.lua
+++ init.lua
@@ -121,24 +121,28 @@ local config = {
     aerial = {
       attach_mode = "global",
       backends = { "lsp", "treesitter", "markdown" },
-      min_width = 28,
+      disable_max_lines = 30000,
+      layout = {
+        min_width = 28,
+      },
       show_guides = true,
       filter_kind = false,
-      disable_max_lines = 30000,
       guides = {
         mid_item = "├ ",
         last_item = "└ ",
         nested_top = "│ ",
         whitespace = "  ",
       },
-      on_attach = function(bufnr)
-        -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set("n", "{", "<cmd>AerialPrev<cr>", { buffer = bufnr, desc = "Jump backwards in Aerial" })
-        vim.keymap.set("n", "}", "<cmd>AerialNext<cr>", { buffer = bufnr, desc = "Jump forwards in Aerial" })
-        -- Jump up the tree with '[[' or ']]'
-        vim.keymap.set("n", "[[", "<cmd>AerialPrevUp<cr>", { buffer = bufnr, desc = "Jump up and backwards in Aerial" })
-        vim.keymap.set("n", "]]", "<cmd>AerialNextUp<cr>", { buffer = bufnr, desc = "Jump up and forwards in Aerial" })
-      end,
+      keymaps = {
+        ["[y"] = "actions.prev",
+        ["]y"] = "actions.next",
+        ["[Y"] = "actions.prev_up",
+        ["]Y"] = "actions.next_up",
+        ["{"] = false,
+        ["}"] = false,
+        ["[["] = false,
+        ["]]"] = false,
+      },
     }
   },
 
