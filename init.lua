90ef228c23062e26de5bfcb0be530be1a963ec84
René Ribaud <rribaud@redhat.com>
2023-01-02 11:35:18 +0100

    fix: Only overload disable_max_lines for aerial
    - Only add specific configuration for aerial.

--- init.lua
+++ init.lua
@@ -308,30 +308,30 @@ local config = {
       -- ensure_installed = { "python" },
     },
     aerial = {
-      attach_mode = "global",
-      backends = { "lsp", "treesitter", "markdown" },
+      -- attach_mode = "global",
+      -- backends = { "lsp", "treesitter", "markdown" },
       disable_max_lines = 30000,
-      layout = {
-        min_width = 28,
-      },
-      show_guides = true,
-      filter_kind = false,
-      guides = {
-        mid_item = "├ ",
-        last_item = "└ ",
-        nested_top = "│ ",
-        whitespace = "  ",
-      },
-      keymaps = {
-        ["[y"] = "actions.prev",
-        ["]y"] = "actions.next",
-        ["[Y"] = "actions.prev_up",
-        ["]Y"] = "actions.next_up",
-        ["{"] = false,
-        ["}"] = false,
-        ["[["] = false,
-        ["]]"] = false,
-      },
+      -- layout = {
+      --   min_width = 28,
+      -- },
+      -- show_guides = true,
+      -- filter_kind = false,
+      -- guides = {
+      --   mid_item = "├ ",
+      --   last_item = "└ ",
+      --   nested_top = "│ ",
+      --   whitespace = "  ",
+      -- },
+      -- keymaps = {
+      --   ["[y"] = "actions.prev",
+      --   ["]y"] = "actions.next",
+      --   ["[Y"] = "actions.prev_up",
+      --   ["]Y"] = "actions.next_up",
+      --   ["{"] = false,
+      --   ["}"] = false,
+      --   ["[["] = false,
+      --   ["]]"] = false,
+      -- },
     }
   },
 
