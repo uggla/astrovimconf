50fb3a9b66d7a7d92a515a7f3cf9c2c30d3621f7
René Ribaud <rribaud@redhat.com>
2024-03-21 16:59:00 +0100

    feat: Add "Refactor" description for <learder>r group

--- mappings.lua
+++ mappings.lua
@@ -21,6 +21,7 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>r"] = { name = "Refactor" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
